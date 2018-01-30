d4644e5c0c0393fd3bceef4a06429c30af804442
subliminalj <jshtz8@mail.umkc.edu>
2018-01-30 17:00:56 -0600

    added anon inner classes for button onClick listeners

--- app/src/main/java/com/jayhixson/umpbuddy/MainActivity.java
+++ app/src/main/java/com/jayhixson/umpbuddy/MainActivity.java
@@ -2,13 +2,29 @@ package com.jayhixson.umpbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
+        private Button mBallButton;
+        private Button mStrikeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    mBallButton = (Button) findViewById(R.id.ball_button);
+    mBallButton.setOnClickListener(new View.OnClickListener(){
+        @Override public void onClick(View v) {
+            // not yet implemented
+        }
+    });
+    mStrikeButton = (Button) findViewById(R.id.strike_button);
+    mStrikeButton.setOnClickListener(new View.OnClickListener(){
+        @Override public void onClick(View v) {
+            // not yet implemented
+        }
+    });
     }
 
 
