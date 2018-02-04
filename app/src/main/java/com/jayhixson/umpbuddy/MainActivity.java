5f6c7f04ac5c4b2e48cee5a9be88f73e0e49f764
subliminalj <jshtz8@mail.umkc.edu>
2018-02-03 19:01:19 -0600

    Outs Balls and Strikes save state

--- app/src/main/java/com/jayhixson/umpbuddy/MainActivity.java
+++ app/src/main/java/com/jayhixson/umpbuddy/MainActivity.java
@@ -16,12 +16,19 @@ public class MainActivity extends AppCompatActivity {
         private Button mStrikeButton;
         private UmpCall mUmpCalls = new UmpCall(0,0);
         private static final String TAG = "UmpBuddy";
+        private static final String KEY_OUTS = "OUTS";
+        private static final String KEY_BALLS = "BALLS";
+        private static final String KEY_STRIKES = "BALLS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
-
+        if (savedInstanceState!=null){
+            mUmpCalls.setOutCount(savedInstanceState.getInt(KEY_OUTS,0));
+            mUmpCalls.setBallCount(savedInstanceState.getInt(KEY_BALLS, 0));
+            mUmpCalls.setStrikeCount(savedInstanceState.getInt(KEY_STRIKES, 0));
+        }
         updateDisplay();
 
     mBallButton = (Button) findViewById(R.id.ball_button);
@@ -64,6 +71,17 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         Log.d(TAG,"onPause() called");
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG,"onSaveInstanceState");
+        savedInstanceState.putInt(KEY_OUTS, mUmpCalls.getOutCount());
+        savedInstanceState.putInt(KEY_BALLS, mUmpCalls.getBallCount());
+        savedInstanceState.putInt(KEY_STRIKES, mUmpCalls.getStrikeCount());
+
+    }
+
     @Override
     public void onStop(){
         super.onStop();
